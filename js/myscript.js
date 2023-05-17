aff20bd3ae55d31a6b6782e292398261312e9392
Ericson-Manalo <ericmana98@gmail.com>
2023-05-17 15:41:39 +0200

    creating mail in array

--- js/myscript.js
+++ js/myscript.js
@@ -18,5 +18,17 @@ if(numPlayer > numComputer){
 } else if ( numPlayer < numComputer){
     console.log ('Il computer ha vinto!');
 }else{
-    console.log ('Pareggio!');
+    console.log ('Avete fatto pareggio!');
 }
+
+
+
+/* -------------------------------------------------------------
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Tenete presente che non è permesso utilizzare funzioni built-in degli array come Array.includes() e Array.indexOf(), pena l'annullamento dell'esercizio.
+****************************************************************/
+
+const arrayMail = ["ciao@gmail.com", "topogigo@yahoo.it", "ironman@hotmail.com", "hulk@libero.it", "drstrange@yahoo.it", "captainamerica1@gmail.com", "spiderman@libero.it", "vedovaneretta@hotmail.it"];
+
+
